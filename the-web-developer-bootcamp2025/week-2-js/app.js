05e9cf026934b84ce8d3b7ce39edae0fc1bb33f9
Lillian Yang <nopeyangli@163.com>
2025-03-10 17:08:16 +1100

    guess what number it is

--- the-web-developer-bootcamp2025/week-2-js/app.js
+++ the-web-developer-bootcamp2025/week-2-js/app.js
@@ -1,1 +1,27 @@
-console.log("Hello from our first JS file!!");
+let maximum = parseInt(prompt("Enter the maximum number"));
+while (!maximum) {
+  maximum = parseInt(prompt("Enter the maximum number"));
+}
+//generates a random integer between 1 and the value of maximum.
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = prompt("Enter your first guess!(Type 'q' to quit)");
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  guess = parseInt(guess);
+  if (guess > targetNum) {
+    guess = prompt("Too high! Enter a new guess:");
+    attempts++;
+  } else if (uess < targetNum) {
+    guess = prompt("Too low! Enter a new guess:");
+    attempts++;
+  } else {
+    guess = prompt("Invalid guess. Please enter a number or 'q' to exit.");
+  }
+}
+
+if (guess === "q") {
+  console.log("ok, you quit!");
+}
