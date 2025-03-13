388926f5fa9c6c2c986acde84098d215a8bb4388
Lillian Yang <nopeyangli@163.com>
2025-03-13 12:12:59 +1100

    add list delete aplied to todos array

--- the-web-developer-bootcamp2025/week-2-js/todoList/todos.js
+++ the-web-developer-bootcamp2025/week-2-js/todoList/todos.js
@@ -1,11 +1,11 @@
 let input;
 const todos = ["Go to CIT", "Taking ENSA class"];
 while (true) {
-  input = prompt("what would you like to do?");
+  input = prompt("what would you like to do?").trim().toLowerCase();
 
   if (input === "quit" || input === "q") {
     console.log("Ok, QUIT the APP!");
-    break; // Exit the loop
+    // Exit the loop
   }
 
   if (input === "list") {
@@ -14,4 +14,24 @@ while (true) {
       console.log(`${i}:${todos[i]}`);
     }
   }
+  if (input === "new") {
+    let newTodo = prompt("Enter a new todo list:");
+    todos.push(newTodo);
+
+    console.log(`”${newTodo}" added to the todo list`);
+    //   break;
+  }
+  if (input === "delete") {
+    let deleteIndex = parseInt(
+      prompt("Please enter the number you want to delete:")
+    );
+    if (!isNaN(deleteIndex) && deleteIndex >= 0 && deleteIndex < todos.length) {
+      let deleteTodo = todos.splice(deleteIndex, 1);
+      console.log(`"${deleteTodo[0]}" has been deleted.`);
+    } else {
+      console.log("Invalid index. Please enter a valid number.");
+    }
+  }
+
+  break;
 }
