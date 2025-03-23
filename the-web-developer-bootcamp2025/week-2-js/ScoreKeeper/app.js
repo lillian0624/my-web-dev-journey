23b9833a0023462e176b7e559f01660e1a2eb54d
Lillian Yang <nopeyangli@163.com>
2025-03-23 15:43:35 +1100

    Score Keeper

--- the-web-developer-bootcamp2025/week-2-js/ScoreKeeper/app.js
+++ the-web-developer-bootcamp2025/week-2-js/ScoreKeeper/app.js
@@ -2,24 +2,53 @@ const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const resetButton = document.querySelector("#reset");
+const winningScoreSelect = document.querySelector("#playTo");
 
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener("click", function () {
-  if (p1Score === winningScore) {
-    console.log("P1 win");
-  } else {
+  if (!isGameOver) {
     p1Score += 1;
     p1Display.textContent = p1Score;
+  }
+  if (p1Score === winningScore) {
+    isGameOver = true;
+    console.log("P1 win");
+    p1Display.classList.add("winner");
+    p2Display.classList.add("loser");
   }
 });
 
 p2Button.addEventListener("click", function () {
-  if (p2Score === winningScore) {
-  } else {
+  if (!isGameOver) {
     p2Score += 1;
     p2Display.textContent = p2Score;
   }
+  if (p2Score === winningScore) {
+    isGameOver = true;
+    console.log("P2 win");
+    p2Display.classList.add("winner");
+    p1Display.classList.add("loser");
+  }
 });
+
+resetButton.addEventListener("click", reset);
+
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("loser", "winner");
+}
