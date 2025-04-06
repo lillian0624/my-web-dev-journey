bdc509db0efdc83954f4d695f4638d650fa1e0d7
Lillian Yang <nopeyangli@163.com>
2025-04-06 17:15:55 +1000

    express send request

--- the-web-developer-bootcamp2025/3-nodejs/FirstApp/index.js
+++ the-web-developer-bootcamp2025/3-nodejs/FirstApp/index.js
@@ -1,12 +1,12 @@
-const { request } = require("express");
-
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
   console.log("We got a new request");
+  //   res.send("Hello, we got your request. This is a response");
+  res.send("<h1>This is my website</h1>");
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("Listening on port 8080");
 });
