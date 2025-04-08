f551e61528964f269af638072a0b7225c97a06e6
Lillian Yang <nopeyangli@163.com>
2025-04-08 20:10:23 +1000

    router

--- the-web-developer-bootcamp2025/3-nodejs/FirstApp/index.js
+++ the-web-developer-bootcamp2025/3-nodejs/FirstApp/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  console.log("We got a new request");
-  //   res.send("Hello, we got your request. This is a response");
-  res.send("<h1>This is my website</h1>");
+// app.use((req, res) => {
+//   console.log("We got a new request");
+//   //   res.send("Hello, we got your request. This is a response");
+//   res.send("<h1>This is my website</h1>");
+// });
+
+app.use("/", (req, res) => {});
+
+app.get("/dogs", (req, res) => {
+  res.send("This si the Home page");
+});
+app.get("/cats", (req, res) => {
+  res.send("Meow Meow");
 });
 
 app.listen(8080, () => {
