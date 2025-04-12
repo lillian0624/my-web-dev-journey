4f936b464292e1eb62cd810f1fcaf120308cee6f
Lillian Yang <nopeyangli@163.com>
2025-04-13 07:06:52 +1000

    ejs

--- the-web-developer-bootcamp2025/3-nodejs/Templating/index.js
+++ the-web-developer-bootcamp2025/3-nodejs/Templating/index.js
@@ -7,7 +7,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("Home.ejs");
+  res.render("home.ejs");
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random.ejs", { rand: num });
 });
 
 app.listen(3000, () => {
