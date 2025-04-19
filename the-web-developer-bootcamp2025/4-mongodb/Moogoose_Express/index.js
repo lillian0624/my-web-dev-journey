554136c376faa64bd92f5975efceded5779fd004
Lillian Yang <nopeyangli@163.com>
2025-04-19 15:24:59 +1000

    setup

--- the-web-developer-bootcamp2025/4-mongodb/Moogoose_Express/index.js
+++ the-web-developer-bootcamp2025/4-mongodb/Moogoose_Express/index.js
@@ -1,2 +1,33 @@
 // npm init -y
 // //npm i express mongoose ejs
+
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/shopApp", {
+    useNewUrlParser: true,
+  })
+
+  .then(() => {
+    console.log("Mongo Connection open!");
+  })
+  .catch((err) => {
+    console.log("Mongo Connection Error!");
+    console.log(err);
+  });
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/dog", (req, res) => {
+  res.send("home");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+//nodemon index.js
