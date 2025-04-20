8345bb0685e1e9848a347cd3ccd126b29d8dfd05
Lillian Yang <nopeyangli@163.com>
2025-04-20 21:13:49 +1000

    add a product form

--- the-web-developer-bootcamp2025/4-mongodb/Moogoose_Express/index.js
+++ the-web-developer-bootcamp2025/4-mongodb/Moogoose_Express/index.js
@@ -30,6 +30,10 @@ app.get("/products", async (req, res) => {
   res.render("products/index", { products });
 });
 
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
