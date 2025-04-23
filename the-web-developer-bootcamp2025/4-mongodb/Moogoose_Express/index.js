462ddbb3ee37e0f59bf25917d18e878b7d38da3a
Lillian Yang <nopeyangli@163.com>
2025-04-23 12:23:13 +1000

    edit form

--- the-web-developer-bootcamp2025/4-mongodb/Moogoose_Express/index.js
+++ the-web-developer-bootcamp2025/4-mongodb/Moogoose_Express/index.js
@@ -52,6 +52,12 @@ app.get("/products/:id", async (req, res) => {
   // res.send("details page!");
   res.render("products/show", { product });
 });
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  const categories = ["Fruit", "Vegetable", "Dairy", "Meat", "Grain"];
+  res.render("products/edit", { product, categories });
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
